7365ac54f23260cd67a7ab07896996503e42c5c3
DongHyeon Kim <hyeoni369@naver.com>
2023-05-09 22:48:47 +0900

    cors 수정

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 from sentencesplitter import split_sentences
 
@@ -8,6 +9,18 @@ from sentencesplitter import split_sentences
 # start dev server with `uvicorn main:app --reload`
 # start prod server with `uvicorn main:app`
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Sentences(BaseModel):
