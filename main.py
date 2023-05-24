bdef6f933e263b20cfa003a043df6b7a67390477
DongHyeon Kim <hyeoni369@naver.com>
2023-05-24 23:53:08 +0900

    slide texts로 받아서 처리할 수 있게 변경

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from typing import List
 
 from fastapi import FastAPI
+from kss import split_sentences
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 
-from sentencesplitter import split_sentences
-
-# pip install fastapi[all]
+# pip install "fastapi[all]"
 
 # start dev server with `uvicorn main:app --reload`
 # start prod server with `uvicorn --host 0.0.0.0 --port 8080 --workers 4 main:app`
@@ -27,10 +26,25 @@ app.add_middleware(
 )
 
 
-class Sentences(BaseModel):
-    sentences: List[str]
+class SlideText(BaseModel):
+    slideId: str
+    slideIndex: int
+    text: str
+
+
+class SlideTexts(BaseModel):
+    slideTexts: List[SlideText]
 
 
 @app.post('/sentence-split')
-def read_item(sentences: Sentences):
-    return {'sentences': split_sentences(sentences.sentences)}
+def read_item(slide_texts: SlideTexts) -> SlideTexts:
+    kss_split_result: List[SlideText] = [
+        SlideText(
+            slideId=slideText.slideId,
+            slideIndex=slideText.slideIndex,
+            text=sentence
+        )
+        for slideText in slide_texts.slideTexts
+        for sentence in split_sentences(slideText.text)
+    ]
+    return SlideTexts(slideTexts=kss_split_result)
